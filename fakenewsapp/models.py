0431b97efcebfa76c98517af5154a13b527bed53
taahcaroline <2002041@aluno.univesp.br>
2022-10-16 14:00:50 -0300

    melhorias

--- fakenewsapp/models.py
+++ fakenewsapp/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Formulário de cadastro de noticias
 class Noticiascad(models.Model):
     titulo = models.CharField('Titulo da notícia', max_length=200)
-    autenticidade = models.CharField('Verdadeiro ou Falso?', max_length=10)
-    linknews = models.URLField('Link', max_length=200)
+    autenticidade = models.BooleanField('Marque se for verdadeira, desmarque se for falsa', default=False)
+    linknews = models.URLField('Link', max_length=200, blank=True, null=True)
     
 
     def __str__(self):
